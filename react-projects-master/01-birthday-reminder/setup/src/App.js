9d6d144b864d38c584645ca635947cee5fc495df
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-20 15:48:32 +0430

    birthday-project

--- react-projects-master/01-birthday-reminder/setup/src/App.js
+++ react-projects-master/01-birthday-reminder/setup/src/App.js
@@ -3,13 +3,13 @@ import data from "./data";
 import List from "./List";
 
 function App() {
-  const [people, setPeople] = useState(data)
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className="container">
-        <h3>0 birthday today</h3>
-        <List people={people}/>
-        <button onClick={() => console.log("click me")}>clear all</button>
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
   );
--- react-projects-master/01-birthday-reminder/setup/src/List.js
+++ react-projects-master/01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-const List = ({people}) => {
-  return(
-    {people.map((person)=> {
-      
-    })}
-  )
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, image, age } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
 };
 
 export default List;
