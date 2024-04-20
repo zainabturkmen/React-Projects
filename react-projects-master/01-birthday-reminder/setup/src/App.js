931d5f67646e3d66db8a1bc232c997a4aab33715
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-20 14:38:15 +0430

    birthday-project

--- react-projects-master/01-birthday-reminder/setup/src/App.js
+++ react-projects-master/01-birthday-reminder/setup/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
-        <List people={people}/>
+        <List people={people} />
         <button onClick={() => console.log("clear me")}>clear all</button>
       </section>
     </main>
