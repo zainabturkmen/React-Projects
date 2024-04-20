1e9e95238445d773152c4125396869ea617e269e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-20 15:38:31 +0430

    birthday-project

--- react-projects-master/01-birthday-reminder/setup/src/App.js
+++ react-projects-master/01-birthday-reminder/setup/src/App.js
@@ -3,12 +3,12 @@ import data from "./data";
 import List from "./List";
 
 function App() {
-  const 
+  const [people, setPeople] = useState(data)
   return (
     <main>
       <section className="container">
         <h3>0 birthday today</h3>
-        <List />
+        <List people={people}/>
         <button onClick={() => console.log("click me")}>clear all</button>
       </section>
     </main>
