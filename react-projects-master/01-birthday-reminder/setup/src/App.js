74bf04b0672b692bfcca510c6480403cb49b4094
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-20 14:23:33 +0430

    birthday-project

--- react-projects-master/01-birthday-reminder/setup/src/App.js
+++ react-projects-master/01-birthday-reminder/setup/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data)
   return (
     <main>
       <section className="container">
