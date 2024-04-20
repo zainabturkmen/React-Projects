70611366b3b18487f5a36ba28e30b5fa4195dd1f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-20 17:14:20 +0430

    Tour-peoject

--- react-projects-master/02-tours/setup/src/Tours.js
+++ react-projects-master/02-tours/setup/src/Tours.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ Tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>ours tours</h2>
+        <div className="underline">
+          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
