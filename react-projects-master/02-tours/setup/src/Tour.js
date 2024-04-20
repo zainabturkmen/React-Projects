e9d35d20ec23a96a753e241faabc32ff36577516
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-20 17:22:41 +0430

    Tour-peoject

--- react-projects-master/02-tours/setup/src/Tour.js
+++ react-projects-master/02-tours/setup/src/Tour.js
@@ -2,7 +2,15 @@ import React, { useState } from "react";
 
 const Tour = ({ id, name, info, image, price }) => {
   return (
-    
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+      </footer>
+    </article>
   );
 };
 
