2ddff79e14d3ca6263950808860bdac94bfa8272
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 09:43:37 +0430

    Tour-peoject

--- react-projects-master/03-reviews/setup/src/Review.js
+++ react-projects-master/03-reviews/setup/src/Review.js
@@ -6,6 +6,29 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length -1
+    }
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return newIndex;
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return newIndex;
+    });
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -18,10 +41,10 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
