0e4b6b6a6f156b9d67e22f4e5ef9cfebd8b956a3
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 09:06:33 +0430

    Tour-peoject

--- react-projects-master/03-reviews/setup/src/Review.js
+++ react-projects-master/03-reviews/setup/src/Review.js
@@ -9,7 +9,10 @@ const Review = () => {
   return (
     <article className="review">
       <div className="img-container">
-        <img src={image} alt={name} />
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
       </div>
     </article>
   );
