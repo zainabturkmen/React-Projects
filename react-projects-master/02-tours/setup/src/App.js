e411b01154e160d98853ebc8be06cf4b668783f4
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 07:41:26 +0430

    Tour-peoject

--- react-projects-master/02-tours/setup/src/App.js
+++ react-projects-master/02-tours/setup/src/App.js
@@ -46,7 +46,9 @@ function App() {
   if (tours.length === 0) {
     return (
       <main>
-        <div className="title"></div>
+        <div className="title">
+          <h2>no tours left</h2>
+        </div>
       </main>
     );
   }
