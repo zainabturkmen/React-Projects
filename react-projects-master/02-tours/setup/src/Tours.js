a6f0edbbc897dd62dcd91148e39955157b7775db
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 08:23:35 +0430

    Tour-peoject

--- react-projects-master/02-tours/setup/src/Tours.js
+++ react-projects-master/02-tours/setup/src/Tours.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Tour from "./Tour";
 const Tours = ({ tours, removeTour }) => {
   return (
     <section>
       <div className="title">
-        <h2>ours tours</h2>
+        <h2>our tours</h2>
         <div className="underline"></div>
       </div>
       <div>
-          {tours.map((tour) => {
-            return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
-          })}
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
       </div>
     </section>
   );
