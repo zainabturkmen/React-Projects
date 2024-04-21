6bf4a8e562c0e34fa249b234e15a9625bd24160b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 09:16:59 +0430

    Tour-peoject

--- react-projects-master/03-reviews/setup/src/Review.js
+++ react-projects-master/03-reviews/setup/src/Review.js
@@ -18,13 +18,14 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="next-btn">
-          <FaChevronRight />
-        </button>
         <button className="prev-btn">
           <FaChevronLeft />
         </button>
+        <button className="next-btn">
+          <FaChevronRight />
+        </button>
       </div>
+      <button className="random-btn">surpeise me</button>
     </article>
   );
 };
