ac1561e030794e2e75f08947af4c0dda4d811f4b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 10:30:24 +0430

    Question-project

--- react-projects-master/04-accordion/setup/src/App.js
+++ react-projects-master/04-accordion/setup/src/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import data from "./data";
 import SingleQuestion from "./Question";
 function App() {
-  const [questions, setQuestions] = useState(0)
-  return(
+  const [questions, setQuestions] = useState(data);
 
-  )
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
