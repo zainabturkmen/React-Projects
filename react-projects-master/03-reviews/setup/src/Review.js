29a9f90e5c4d213f91f8b505f398bc889ed15308
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 09:03:02 +0430

    Tour-peoject

--- react-projects-master/03-reviews/setup/src/Review.js
+++ react-projects-master/03-reviews/setup/src/Review.js
@@ -5,8 +5,10 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
-  
-  return <h2>review component</h2>;
+
+  return <article>
+    
+  </article>;
 };
 
 export default Review;
