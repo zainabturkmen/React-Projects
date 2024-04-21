bdf0ead1e40bd3b5e00c8922752c75fe09837f55
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 08:55:35 +0430

    Tour-peoject

--- react-projects-master/03-reviews/setup/src/Review.js
+++ react-projects-master/03-reviews/setup/src/Review.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
+  const [index, setIndex] = useState(0)
   return <h2>review component</h2>;
 };
 
