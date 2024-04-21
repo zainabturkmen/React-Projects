70fcf00ad49c95f5ed45fd8a30d4cd6bc29ea0f0
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-21 15:11:44 +0430

    tabs project

--- react-projects-master/06-tabs/final/src/App.js
+++ react-projects-master/06-tabs/final/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+// I SWITCHED TO PERMANENT DOMAIN 
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
   const [loading, setLoading] = useState(true)
