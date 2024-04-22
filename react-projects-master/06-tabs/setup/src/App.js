ce954db3ead90d0f534bd68ee8b5f62c814286ea
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-22 08:55:30 +0430

    tabs project

--- react-projects-master/06-tabs/setup/src/App.js
+++ react-projects-master/06-tabs/setup/src/App.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [jobs, setJobs] = useState([])
-  const [value, setValue] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
 
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
 
-const fetchJobs = async () => {
-  const response = await fetch(url);
-  const newJobs = await response.json();
-  setJobs(newJobs)
-  setLoading(false)
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+  return <h2>Jobs</h2>;
 }
 
-useEffect(() => {
-  fetchJobs()
-}, [])
-
-if (loading) {
-  return <section>
-    
-  </section>
-}
-  return <h2>Jobs</h2>
-}
-
-export default App
+export default App;
