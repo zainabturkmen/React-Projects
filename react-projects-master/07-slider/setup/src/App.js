338b36a3cb35125bad768f154a149e8652ba5143
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-22 12:17:12 +0430

    slider

--- react-projects-master/07-slider/setup/src/App.js
+++ react-projects-master/07-slider/setup/src/App.js
@@ -1,10 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  const [people, setPeople] = useState(data)
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default App;
