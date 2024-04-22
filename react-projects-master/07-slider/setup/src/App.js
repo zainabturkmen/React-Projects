83417173d38d438dba724269f200a85892e871ff
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-22 12:26:52 +0430

    slider

--- react-projects-master/07-slider/setup/src/App.js
+++ react-projects-master/07-slider/setup/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-import { FaQuoteRight } from "react-icons/fa";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
@@ -21,8 +21,15 @@ function App() {
             <h4>{name}</h4>
             <p className="title">{title}</p>
             <p className="text">{quote}</p>
+            <FaQuoteRight className="icon"/>
           </article>
         })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
       </div>
     </section>
   );
