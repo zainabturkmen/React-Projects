1363d65fd1b1255d045eec5420f6429eb9d7c604
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 15:22:02 +0430

    color generator

--- react-projects-master/09-color-generator/setup/src/SingleColor.js
+++ react-projects-master/09-color-generator/setup/src/SingleColor.js
@@ -5,7 +5,9 @@ const SingleColor = ({rgb, weight, index}) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",")
   return (
-    <article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>{''}</article>
+    <article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
+      <p className="percent-value">{weight}</p>
+    </article>
   )
 }
 
