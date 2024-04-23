99c15b28e99a32be7af2ed59ee0acc6cf3a4ead4
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 15:07:13 +0430

    color generator

--- react-projects-master/09-color-generator/setup/src/SingleColor.js
+++ react-projects-master/09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index}) => {
+  return ()
 }
 
 export default SingleColor
