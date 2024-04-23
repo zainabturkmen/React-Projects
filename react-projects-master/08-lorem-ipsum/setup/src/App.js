07a4edb4380c591dbbcfbf144c134d4d2f292e87
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 11:11:32 +0430

    lorem ipsum

--- react-projects-master/08-lorem-ipsum/setup/src/App.js
+++ react-projects-master/08-lorem-ipsum/setup/src/App.js
@@ -1,26 +1,24 @@
-import React, { useState } from 'react';
-import data from './data';
-
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
-  
-  const [count, setCount] = useState(0)
-  const [text, setText] = useState([])
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-  const handleSubmit = (e)=>{
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     console.log("hello world");
-  }
+  };
 
   return (
-    <section className = "section-center">
+    <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
       <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">Paragraphs:</label>
-        <input type="number" name="amount" id="amount" />
+        <input type="number" name="amount" id="amount" value={count} onChange={(e)=> setCount(e.target.value)}/>
       </form>
     </section>
-    );
+  );
 }
 
 export default App;
