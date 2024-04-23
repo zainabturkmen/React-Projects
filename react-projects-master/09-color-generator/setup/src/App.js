2b11eea9e51a684f3a7cca733f160fa47857580c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 12:18:31 +0430

    color generator

--- react-projects-master/09-color-generator/setup/src/App.js
+++ react-projects-master/09-color-generator/setup/src/App.js
@@ -4,9 +4,10 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-
+ const [color, setColor] = useState("")
+ const [error, setError] = useState(false )
   return <h2>color generator project</h2>
-  
+
 }
 
 export default App
