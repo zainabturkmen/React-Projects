fd9f10ec9276811bf649159938205ff2452f95ad
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 11:05:59 +0430

    lorem ipsum

--- react-projects-master/08-lorem-ipsum/setup/src/App.js
+++ react-projects-master/08-lorem-ipsum/setup/src/App.js
@@ -7,8 +7,8 @@ function App() {
   const [count, setCount] = useState(0)
   const [text, setText] = useState([])
 
-  const handleSubmit = ()=>{
-
+  const handleSubmit = (e)=>{
+    e.preventDefault()
   }
 
   return (
