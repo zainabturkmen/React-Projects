71fd85c1292545d6106c7e1d9bcff1cd684af37a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 10:11:11 +0430

    lorem ipsum

--- react-projects-master/08-lorem-ipsum/setup/src/App.js
+++ react-projects-master/08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import data from './data';
+
+
 function App() {
   
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
   
   return (
-  <h2>lorem ipsum project setup</h2>
+    <h2>lorem ipsum project setup</h2>
     )
 }
 
