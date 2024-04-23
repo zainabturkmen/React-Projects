0c53a114de8a7f242727ac32b27b74b7fa925404
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 15:14:03 +0430

    color generator

--- react-projects-master/09-color-generator/setup/src/SingleColor.js
+++ react-projects-master/09-color-generator/setup/src/SingleColor.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({rgb, weight, index}) => {
-  return ()
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",")
+  return (
+    <article className={`color`}></article>
+  )
 }
 
 export default SingleColor
