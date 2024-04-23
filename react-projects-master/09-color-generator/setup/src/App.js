30b687eb69001a4f0a4ad8a5e53f8dad7ecf5384
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 12:31:40 +0430

    color generator

--- react-projects-master/09-color-generator/setup/src/App.js
+++ react-projects-master/09-color-generator/setup/src/App.js
@@ -18,7 +18,7 @@ function App() {
       <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" Values={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025"/>
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025"/>
           <button className="btn" type="submit">submit</button>
         </form>
       </section>
