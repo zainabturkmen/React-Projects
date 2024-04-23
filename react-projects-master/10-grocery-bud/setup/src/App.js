d7dcd4072e690cc71c7d377ec56fc73dc307582a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 16:56:32 +0430

    grocery bud

--- react-projects-master/10-grocery-bud/setup/src/App.js
+++ react-projects-master/10-grocery-bud/setup/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [list, setList] = useState([])
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
-  const [alert, setAlert] = useState({})
+  const [alert, setAlert] = useState({show:false, msg:"", type:""})
   return <h2>grocery bud setup</h2>
 }
 
