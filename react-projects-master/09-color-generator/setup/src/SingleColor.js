08e30440c8c0955f6fa4a024b064bd2cdac824af
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 16:00:50 +0430

    color generator

--- react-projects-master/09-color-generator/setup/src/SingleColor.js
+++ react-projects-master/09-color-generator/setup/src/SingleColor.js
@@ -5,10 +5,11 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",")
   const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`
   return (
     <article className={`color ${index > 10 && "color-light"}`} style={{ backgroundColor: `rgb(${bcg})` }}>
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">#{hexColor}</p>
+      <p className="color-value">{hexValue}</p>
     </article>
   );
 }
