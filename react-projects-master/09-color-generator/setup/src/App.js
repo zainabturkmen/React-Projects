13e8c9312df7329e0addab67f5e1cf40e976834a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-23 12:27:43 +0430

    color generator

--- react-projects-master/09-color-generator/setup/src/App.js
+++ react-projects-master/09-color-generator/setup/src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
- const [color, setColor] = useState("")
- const [error, setError] = useState(false)
- const [list, setList] = useState([])
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
 
- const handleSubmit = (e) => {
-  e.preventDefault()
- }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
 
-  return <h2>color generator project</h2>
-
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" Values={color} onChange={(e) => setColor(e.target.value)} />
+          <button className="btn" type="submit">submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
