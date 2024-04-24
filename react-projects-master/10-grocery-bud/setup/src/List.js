4476820e4f161c7dfd3e3813808855173bb9d692
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-24 11:03:03 +0430

    grocery bud

--- react-projects-master/10-grocery-bud/setup/src/List.js
+++ react-projects-master/10-grocery-bud/setup/src/List.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items}) => {
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items }) => {
   return (
-    <div className='grocery-list'>
-      {items.map((item)=> {
-        const {id, title} = item;
-        return()
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+          </article>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
