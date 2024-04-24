38a192d426b4173ae2c2185e0d618f973c940af7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-24 11:55:10 +0430

    grocery bud

--- react-projects-master/10-grocery-bud/setup/src/App.js
+++ react-projects-master/10-grocery-bud/setup/src/App.js
@@ -17,6 +17,7 @@ function App() {
     e.preventDefault();
     if (!name) {
       // display alert
+      setAlert({show:true, msg:"please enter value"})
     } else if (name && isEditing) {
       // deall with edit
     } else {
