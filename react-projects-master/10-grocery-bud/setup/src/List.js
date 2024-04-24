188128ea26776f8c84e0169a7cf7bb0a29c58151
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-24 11:01:46 +0430

    grocery bud

--- react-projects-master/10-grocery-bud/setup/src/List.js
+++ react-projects-master/10-grocery-bud/setup/src/List.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items}) => {
+  return (
+    <div className='grocery-list'>
+      {items.map((item)=> {
+        const {id, title} = item;
+        return()
+      })}
+    </div>
+  )
 }
 
 export default List
