f4cf71dee2e368bd8a4b89343fc4e92aa2ac18b0
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-24 11:26:16 +0430

    grocery bud

--- react-projects-master/10-grocery-bud/setup/src/App.js
+++ react-projects-master/10-grocery-bud/setup/src/App.js
@@ -1,49 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  const [name, setName] = useState('')
-  const [list, setList] = useState([])
-  const [isEditing, setIsEditing] = useState(false)
-  const [editID, setEditID] = useState(null)
-  const [alert, setAlert] = useState({show:true, msg:"hello world", type:"success"})
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: true,
+    msg: "hello world",
+    type: "success",
+  });
 
-  const handleSubmit = (e)=> {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (!name) {
       // display alert
-    }
-    else if(name && isEditing){
+    } else if (name && isEditing) {
       // deall with edit
-    }
-    else{
+    } else {
       // show alert
-      const newItem = {id: new Date().getTime().toString, title:name}
+      const newItem = { id: new Date().getTime().toString, title: name };
       setList([...list, newItem]);
-      setName("")
+      setName("");
     }
-  }
+  };
   return (
-    <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
         {alert.show && <Alert />}
         <h3>grocery bud</h3>
-        <div className='form-control'>
-          <input type='text' className='grocery' placeholder='e.g eggs' value={name} onChange={(e) => setName(e.target.value)}/>
-          <button type='submit' className='submit-btn'>
-            {isEditing? "edit": "submit"}
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
           </button>
         </div>
       </form>
       {list.length > 0 && (
-       <div className='grocery-container'>
-        <List items= {list}/>
-        <button className='clear-btn'>clear items</button>
-       </div>
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
       )}
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
