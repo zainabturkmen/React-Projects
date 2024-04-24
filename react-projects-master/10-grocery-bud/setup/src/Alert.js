2a7ccf1e806b9007199659c1fc45566ff993dd48
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-24 13:17:03 +0430

    grocery bud

--- react-projects-master/10-grocery-bud/setup/src/Alert.js
+++ react-projects-master/10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react'
 
 const Alert = ({ type, msg, removeAlert }) => {
-  useEffect(())
+  useEffect(()=> {
+    const timout = setTimeout(()=> {
+      removeAlert()
+    }, 3000)
+  },[])
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
