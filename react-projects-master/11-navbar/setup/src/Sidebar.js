53f4a8ac8532773280286037055761f7be953913
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-25 16:36:05 +0430

    navbar

--- react-projects-master/11-navbar/setup/src/Sidebar.js
+++ react-projects-master/11-navbar/setup/src/Sidebar.js
@@ -3,18 +3,20 @@ import { links, social } from "./data";
 
 const Sidebar = () => {
   return (
-    <div className="links-container show-container">
-      <ul className="links">
-        {links.map((link) => {
-          const { id, url, text } = link;
-          return (
-            <li key={id}>
-              <a href={url}>{text}</a>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <div className="links-container show-container">
+        <ul className="links">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
 
