f92b7c40f64718c5eacc23bae7edce0b68ecb3dc
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-25 18:02:17 +0430

    navbar

--- react-projects-master/11-navbar/setup/src/Navbar.js
+++ react-projects-master/11-navbar/setup/src/Navbar.js
@@ -18,7 +18,20 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && }
+        {showLinks && (
+          <div className="links-container show-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
         <div className="links-container show-container">
           <ul className="links">
             {links.map((link) => {
