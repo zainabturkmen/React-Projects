a2a9a65e991eaf3f7870d6b89be4288983b151ce
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-25 16:29:37 +0430

    navbar

--- react-projects-master/11-navbar/setup/src/Sidebar.js
+++ react-projects-master/11-navbar/setup/src/Sidebar.js
@@ -1,21 +1,11 @@
 import React from  "react"
+import {links, social} from "./data"
 
 const Sidebar = () => {
     return (
       <div className="links-container show-container">
         <ul className="links">
-          <li>
-            <a href="#">home</a>
-          </li>
-          <li>
-            <a href="#">about</a>
-          </li>
-          <li>
-            <a href="#">contact</a>
-          </li>
-          <li>
-            <a href="#">products</a>
-          </li>
+            {}
         </ul>
       </div>
     );
