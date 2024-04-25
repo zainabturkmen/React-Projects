6ab975f68d683e6e965a5e1b31faa2a1f9812308
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-25 16:07:07 +0430

    grocery bud

--- react-projects-master/11-navbar/setup/src/Navbar.js
+++ react-projects-master/11-navbar/setup/src/Navbar.js
@@ -13,22 +13,7 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
-          </ul>
-        </div>
+        
         <ul className="social-icons">
           <li>
             <a href="https://www.twitter.com">
