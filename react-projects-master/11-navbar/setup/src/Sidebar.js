f4266b1aad994c7af246acd79b53dc7389392921
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-25 16:33:15 +0430

    navbar

--- react-projects-master/11-navbar/setup/src/Sidebar.js
+++ react-projects-master/11-navbar/setup/src/Sidebar.js
@@ -1,21 +1,21 @@
-import React from  "react"
-import {links, social} from "./data"
+import React from "react";
+import { links, social } from "./data";
 
 const Sidebar = () => {
-    return (
-      <div className="links-container show-container">
-        <ul className="links">
-         {links.map((link)=> {
-            const {id, url, text} = link
-            return (
-                <li key={id}>
-                    <a href={url}>{text}</a>
-                </li>
-            )
-         })}
-        </ul>
-      </div>
-    );
-}
+  return (
+    <div className="links-container show-container">
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default Sidebar;
