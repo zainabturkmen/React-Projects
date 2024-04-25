a755334eaeab6f8f36456796d14e1fb5a43cde48
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-25 16:21:21 +0430

    navbar

--- react-projects-master/11-navbar/setup/src/Navbar.js
+++ react-projects-master/11-navbar/setup/src/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        
+        {}
         <ul className="social-icons">
           <li>
             <a href="https://www.twitter.com">
