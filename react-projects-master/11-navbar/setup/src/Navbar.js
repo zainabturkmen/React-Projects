b8c572f81a13075032f269624c37e9a1eab5ea55
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-26 11:31:57 +0430

    navbar

--- react-projects-master/11-navbar/setup/src/Navbar.js
+++ react-projects-master/11-navbar/setup/src/Navbar.js
@@ -9,9 +9,12 @@ const Navbar = () => {
   const linksRef = useRef(null)
 
   useEffect(()=> {
-    const linksHeight = linksRef.current.getBoundingClientRect().height();
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
     if (showLinks) {
-      linksContainerRef.current.style.height = `${linksHeight}`
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    }
+    else{
+      linksContainerRef.current.style.height = "0px"
     }
   }, [showLinks])
 
