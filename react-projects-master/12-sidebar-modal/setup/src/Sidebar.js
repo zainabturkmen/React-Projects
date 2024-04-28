7384e3667c4119511fc933f7a5ea24e52ab67ffa
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 09:50:43 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/Sidebar.js
+++ react-projects-master/12-sidebar-modal/setup/src/Sidebar.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
 
 const Sidebar = () => {
-  return <aside className={`sidebar show-sidebar`}>
-    <div className='sidebar-header'>
-      <img src={logo} className='logo' alt='coding addict' />
-      <button className='close-btn'>
-        <FaTimes />
-      </button>
-    </div>
-    <ul className='links'>
-      {links.map((link)=> {
-        const {id, url, text, icon} = link
-        return(
-          
-        )
-      })}
-    </ul>
-  </aside>
-}
+  return (
+    <aside className={`sidebar show-sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="coding addict" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
