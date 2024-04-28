6888cd53b4b588aeb1d5643f350910dcd5874f82
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 15:52:49 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/context.js
+++ react-projects-master/12-sidebar-modal/setup/src/context.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 
 const AppContext = React.createContext();
+const [isSidebar, setIsSidebar] = useState()
 
 const AppProvider = ({ children }) => {
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
