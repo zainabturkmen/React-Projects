ba499eb519e1c1e1f6c261f3d13f01a3d8aa1731
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 15:54:37 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/context.js
+++ react-projects-master/12-sidebar-modal/setup/src/context.js
@@ -1,11 +1,12 @@
 import React, { useState, useContext } from "react";
 
 const AppContext = React.createContext();
-const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-const [isModalOpen, setIsModalOpen] = useState(false)
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
+    return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
 
 //custom hook
