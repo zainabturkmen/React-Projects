6d569ee639ff999f07c723021ebd2067cae7bba9
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 18:52:20 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/context.js
+++ react-projects-master/13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,36 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    isSidebarOpen(false);
+  };
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    isSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ isSidebarOpen, isSubmenuOpen, openSidebar, closeSidebar, openSubmenu, closeSubmenu }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+};
+
+ 
--- react-projects-master/13-stripe-submenus/setup/src/index.js
+++ react-projects-master/13-stripe-submenus/setup/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {AppProvider} from "./context"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AppProvider></AppProvider>
     <App />
   </React.StrictMode>
 );
