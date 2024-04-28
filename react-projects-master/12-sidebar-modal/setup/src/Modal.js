326fac79bb305c8b2d8cb1987ad4d399c85fc2d5
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 16:56:58 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/Modal.js
+++ react-projects-master/12-sidebar-modal/setup/src/Modal.js
@@ -13,7 +13,7 @@ const Modal = () => {
     >
       <div className="modal-container">
         <h3>modal content</h3>
-        <button className="close-modal-btn">
+        <button className="close-modal-btn" onClick={closeModal}>
           <FaTimes />
         </button>
       </div>
