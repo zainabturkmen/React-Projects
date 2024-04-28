0e67d4b3fa02055790d7b0abbc27ee0b59374209
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 19:17:05 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/Navbar.js
+++ react-projects-master/13-stripe-submenus/setup/src/Navbar.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
-import { useGlobalContext } from './context'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext()
-  return <nav className='nav'>
-    <div className='nav-center'>
-      <div className='nav-header'>
-        <img src={logo} className="nav-logo"  alt='stripe'/>
-        <button className='btn toggle-btn' onClick={openSidebar}>
-          <FaBars />
-        </button>
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="stripe" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">products</button>
+          </li>
+          <li>
+            <button className="link-btn">developer</button>
+          </li>
+          <li>
+            <button className="link-btn">company</button>
+          </li>
+        </ul>
       </div>
-      <ul className='nav-links'>
+    </nav>
+  );
+};
 
-      </ul>
-    </div>
-  </nav>
-}
-
-export default Navbar
+export default Navbar;
