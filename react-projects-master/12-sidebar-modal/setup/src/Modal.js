f333374a2d32e767ee841d99aa8808b3a07ada1b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 09:31:16 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/Modal.js
+++ react-projects-master/12-sidebar-modal/setup/src/Modal.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 const Modal = () => {
-  return <h2>Modal</h2>
+  return <div className={`modal-overlay show-modal`}>
+    <div className='modal-container'>
+      <h3>modal content</h3>
+    </div>
+  </div>
 }
 
 export default Modal
