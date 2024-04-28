c0273a8bd9b7a6ae77329bf3b86aa8fea43d7e9f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 11:30:49 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/Home.js
+++ react-projects-master/12-sidebar-modal/setup/src/Home.js
@@ -3,6 +3,7 @@ import { FaBars } from "react-icons/fa";
 import {AppContext} from "./context"
 
 const Home = () => {
+  const data = useContext(AppContext)
   return (
     <main>
       <button className="sidebar-toggle">
