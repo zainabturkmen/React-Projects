70c2df8a3815dc0a536b8cf3af3121c8546f27c7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 17:00:59 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/Sidebar.js
+++ react-projects-master/12-sidebar-modal/setup/src/Sidebar.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import { FaTimes } from "react-icons/fa";
 import { social, links } from "./data";
+import { useGlobalContext } from "./context";
+
 
 const Sidebar = () => {
   return (
