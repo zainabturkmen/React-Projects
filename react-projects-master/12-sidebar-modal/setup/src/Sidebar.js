dbcdf3376be3b91c385aec7f29f627c9fde542ef
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 09:43:15 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/Sidebar.js
+++ react-projects-master/12-sidebar-modal/setup/src/Sidebar.js
@@ -7,7 +7,9 @@ const Sidebar = () => {
   return <aside className={`sidebar show-sidebar`}>
     <div className='sidebar-header'>
       <img src={logo} className='logo' alt='coding addict' />
-      <button className='close-btn' ></button>
+      <button className='close-btn'>
+        <FaTimes />
+      </button>
     </div>
   </aside>
 }
