7b0974b7da6b36d78243af3adc1375ce246371b8
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-28 09:28:12 +0430

    Sidebar

--- react-projects-master/12-sidebar-modal/setup/src/Home.js
+++ react-projects-master/12-sidebar-modal/setup/src/Home.js
@@ -4,7 +4,10 @@ import { FaBars } from "react-icons/fa";
 const Home = () => {
   return (
     <main>
-      <button className="sidebar-toggle">{FaBars}</button>
+      <button className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button   className="btn">SHOW Modal</button>
     </main>
   );
 };
