3c8cc6b03f89838e2c66b899b06b86a145042e1a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-29 12:19:51 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/Submenu.js
+++ react-projects-master/13-stripe-submenus/setup/src/Submenu.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { useGlobalContext } from './context'
 
 const Submenu = () => {
+  const {isSubmenuOpen} = useGlobalContext()
   return <h2>submenu component</h2>
 }
 
