c9c0bcddb07af5e5c5b1c268dfa86e08196111e8
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-29 12:52:46 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/Navbar.js
+++ react-projects-master/13-stripe-submenus/setup/src/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     const tempBtn = e.target.getBoundingClientRect();
     const center = (tempBtn.left + tempBtn + right)/ 2;
     const bottom = tempBtn.bottom - 3
-    openSubmenup(page,bottom)
+    openSubmenup(page, {center, bottom})
   } 
 
   return (
