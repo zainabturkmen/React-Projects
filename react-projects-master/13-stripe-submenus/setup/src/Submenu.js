d02217191c42bef9516c1b04a357314586d07513
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-29 18:18:32 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/Submenu.js
+++ react-projects-master/13-stripe-submenus/setup/src/Submenu.js
@@ -13,7 +13,7 @@ const Submenu = () => {
   return (
     <aside  className={`${isSubmenuOpen? "submenu show": "submenu"}`} ref={continer}>
        <h4>{page}</h4>
-       <div className={`submenu`}></div>
+       <div className={`submenu-center`}></div>
     </aside>
   )
 }
