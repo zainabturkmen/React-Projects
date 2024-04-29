a2a9b01a4df11ca4a624fc43bea84c24f982a266
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-29 13:06:40 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/Submenu.js
+++ react-projects-master/13-stripe-submenus/setup/src/Submenu.js
@@ -5,7 +5,7 @@ const Submenu = () => {
   const { isSubmenuOpen, location } = useGlobalContext();
   const continer = useRef(null)
   useEffect(()=> {
-
+    const submenu = continer.current
   },[location])
   return (
     <aside  className={`${isSubmenuOpen? "submenu show": "submenu"}`} ref={continer}>
