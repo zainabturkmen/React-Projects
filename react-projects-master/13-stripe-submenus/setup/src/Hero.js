a39b5d8699c34cdc86c75534a9620c007be2c53f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-29 09:49:18 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/Hero.js
+++ react-projects-master/13-stripe-submenus/setup/src/Hero.js
@@ -6,7 +6,9 @@ const Hero = () => {
   const { closeSubmenu } = useGlobalContext();
 
   return <section className='hero'>
-    
+    <div className='hero-center'>
+      <article></article>
+    </div>
   </section>
 }
 
