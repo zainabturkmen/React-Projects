122b11ab7d979bbcac93dd3c4a16415ae1294e59
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-29 10:30:07 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/Sidebar.js
+++ react-projects-master/13-stripe-submenus/setup/src/Sidebar.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
-import {useGlobalContext} from "./context"
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  const { isSidebarOpen, closeSidebar} = useGlobalContext();
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
     <aside
       className={`${
         isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper "
       }`}
     >
-
-      <div className='sidebar'>
-        <button className='close-btn' onClick={closeSidebar}>
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
           <FaTimes />
         </button>
+        <div className="sidebar-links">
+          {sublinks.map((item, index) => {
+            const {links, page} = item
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+              </article>
+            )
+        })}</div>
       </div>
-      
     </aside>
-
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
