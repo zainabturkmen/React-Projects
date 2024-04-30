518cc28cbe7863faee38ebe094f0998f19e3532f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-30 16:43:27 +0430

    cart

--- react-projects-master/14-cart/setup/src/CartContainer.js
+++ react-projects-master/14-cart/setup/src/CartContainer.js
@@ -37,7 +37,7 @@ const CartContainer = () => {
         </div>
         <button
           className="btn clear-btn"
-          onClick={() => console.log("clear cart")}
+          onClick={}
         >
           clear cart
         </button>
