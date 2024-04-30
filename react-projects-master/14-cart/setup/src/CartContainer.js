e23c242e89392eb78d5b7d2665012a6cf71d8eb4
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-30 16:31:32 +0430

    cart

--- react-projects-master/14-cart/setup/src/CartContainer.js
+++ react-projects-master/14-cart/setup/src/CartContainer.js
@@ -3,7 +3,7 @@ import CartItem from './CartItem'
 import { useGlobalContext } from './context'
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
+  const { cart , total } = useGlobalContext()
   if (cart.length === 0) {
     return (
       <section className='cart'>
