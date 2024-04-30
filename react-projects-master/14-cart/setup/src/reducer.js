5ec6dffbc55800de33eaf7ffb60bf9ecea427259
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-30 19:29:15 +0430

    cart

--- react-projects-master/14-cart/setup/src/reducer.js
+++ react-projects-master/14-cart/setup/src/reducer.js
@@ -10,44 +10,48 @@ const reducer = (state, action) => {
   }
 
   if (action.type === "INCREASE") {
-    let tempCart = state.cart.map((cartItems)=> {
-        if (cartItems.id === action.payload) {
-            return {
-                ...cartItems, 
-                amount: cartItems.amount + 1
-            }
-        }
-        return cartItems;
-
-    });
-    return { ...state, cart: tempCart };
-  }
-  
-  if (action.type === "DECREASE") {
     let tempCart = state.cart.map((cartItems) => {
       if (cartItems.id === action.payload) {
         return {
           ...cartItems,
-          amount: cartItems.amount - 1,
+          amount: cartItems.amount + 1,
         };
       }
       return cartItems;
-    }).filter((cartItems) => cartItems.amount !== 0)
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItems) => {
+        if (cartItems.id === action.payload) {
+          return {
+            ...cartItems,
+            amount: cartItems.amount - 1,
+          };
+        }
+        return cartItems;
+      })
+      .filter((cartItems) => cartItems.amount !== 0);
     return { ...state, cart: tempCart };
   }
 
   if (action.type === "GET_TATALS") {
-    const {total, amount} = state.cart.reduce((cartTotal, cartItem) => {}, {
-        total: 0,
-        amount: 0
-    })
+    const { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
+        const {price, amount} = cartItem;
+        cartTotal.amount
+        return cartTotal
+    }, {
+      total: 0,
+      amount: 0,
+    });
 
-    return{
-        ...state,
-        total,
-        amount, 
-
-    }
+    return {
+      ...state,
+      total,
+      amount,
+    };
   }
   return state;
 };
