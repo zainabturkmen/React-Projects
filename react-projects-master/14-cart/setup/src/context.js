2a4a902b3cceb29a979d244b4a2669fcdc1f802b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-30 16:42:06 +0430

    cart

--- react-projects-master/14-cart/setup/src/context.js
+++ react-projects-master/14-cart/setup/src/context.js
@@ -17,13 +17,14 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initailState);
 
   const clearCart = () => {
-    dispatch()
+    dispatch({type: "CLEAR_CART"})
   };
 
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
       }}
     >
       {children}
