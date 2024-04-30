c764f3aadfa7672a3ddea5079e3ebf0759bb5c02
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-04-30 09:12:51 +0430

    Sidebar

--- react-projects-master/13-stripe-submenus/setup/src/Hero.js
+++ react-projects-master/13-stripe-submenus/setup/src/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
   const { closeSubmenu } = useGlobalContext();
 
   return (
-    <section className="hero">
+    <section className="hero" onMouseOver={}>
       <div className="hero-center">
         <article className="hero-info">
           <h1>
