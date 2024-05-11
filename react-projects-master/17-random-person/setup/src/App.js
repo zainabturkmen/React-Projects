2b81592f4cd65369af672685c5e9b33eac2329d8
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-11 16:00:33 +0430

    random-person

--- react-projects-master/17-random-person/setup/src/App.js
+++ react-projects-master/17-random-person/setup/src/App.js
@@ -3,13 +3,17 @@ import {
   FaEnvelopeOpen,
   FaUser,
   FaCalendarTimes,
-  FaMap,
+  FaMap,  
   FaPhone,
   FaLock,
 } from 'react-icons/fa'
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null)
   return <h2>random user starter</h2>
 }
 
