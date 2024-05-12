ff6a3306ccf771bc995d133d59eb598631a03bb8
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-12 14:13:45 +0430

    random-person

--- react-projects-master/18-pagination/setup/src/Follower.js
+++ react-projects-master/18-pagination/setup/src/Follower.js
@@ -5,6 +5,7 @@ const Follower = ({avatar_url, html_url, login}) => {
   return <article className="card">
     <img src={avatar_url} alt={login} />
     <h4>{login}</h4>
+    <a href={html_url}></a>
   </article>
 }
 
