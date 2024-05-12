c6679c1cfa990656d4a94d9a7cbaa39a1795e5d7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-12 13:48:55 +0430

    random-person

--- react-projects-master/18-pagination/setup/src/App.js
+++ react-projects-master/18-pagination/setup/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
+
 function App() {
+  const {loading, data} = useFetch();
   return <h2>pagination starter</h2>
 }
 
