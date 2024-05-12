ff1d1b126e1c601c39ad045e20462ed0757df89d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-12 15:44:09 +0430

    pagination

--- react-projects-master/18-pagination/setup/src/utils.js
+++ react-projects-master/18-pagination/setup/src/utils.js
@@ -1,9 +1,9 @@
 const paginate = (followers) => {
     const itemPerPage = 10;
-    const page = Math.ceil(followers.length / itemPerPage)
+    const pages = Math.ceil(followers.length / itemPerPage)
     console.log(page);
 
-    const newFallowers = 
+    const newFallowers = Array.from({length:pages})
 }
 
 export default paginate
