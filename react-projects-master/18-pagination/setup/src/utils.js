60132b099739f7dda0ff45c3806b918f76686659
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-12 15:40:36 +0430

    random-person

--- react-projects-master/18-pagination/setup/src/utils.js
+++ react-projects-master/18-pagination/setup/src/utils.js
@@ -2,6 +2,8 @@ const paginate = (followers) => {
     const itemPerPage = 10;
     const page = Math.ceil(followers.length / itemPerPage)
     console.log(page);
+
+    const newFallowers = 
 }
 
 export default paginate
