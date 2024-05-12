d5b338259514d66645ce06bcb33a12d3a142edfe
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-12 08:56:52 +0430

    random-person

--- react-projects-master/17-random-person/setup/src/App.js
+++ react-projects-master/17-random-person/setup/src/App.js
@@ -16,8 +16,16 @@ function App() {
   const [title, setTitle] = useState("name");
   const [value, setValue] = useState("random-person");
 
+  const getPerson = async () => {
+
+  };
+
+  useEffect(()=> {
+    
+  })
+
   const handleValue = (e) => {
-    console.log(e.target);  
+    console.log(e.target);
   };
 
   return (
@@ -25,31 +33,55 @@ function App() {
       <div className="block bcg-black"></div>
       <div className="block">
         <div className="container">
-          <img src={(person && person.image) || defaultImage} alt="random user" className="user-img" />
+          <img
+            src={(person && person.image) || defaultImage}
+            alt="random user"
+            className="user-img"
+          />
           <p className="user-title">my {title} is</p>
           <p className="user-value">{value}</p>
           <div className="values-list">
-            <button className="icon" data-label="name" onMouseOver={handleValue}>
+            <button
+              className="icon"
+              data-label="name"
+              onMouseOver={handleValue}
+            >
               <FaUser />
             </button>
-            <button className="icon" data-label="email" onMouseOver={handleValue}>
+            <button
+              className="icon"
+              data-label="email"
+              onMouseOver={handleValue}
+            >
               <FaEnvelopeOpen />
             </button>
             <button className="icon" data-label="age" onMouseOver={handleValue}>
               <FaCalendarTimes />
             </button>
-            <button className="icon" data-label="street" onMouseOver={handleValue}>
+            <button
+              className="icon"
+              data-label="street"
+              onMouseOver={handleValue}
+            >
               <FaMap />
             </button>
-            <button className="icon" data-label="phone" onMouseOver={handleValue}>
+            <button
+              className="icon"
+              data-label="phone"
+              onMouseOver={handleValue}
+            >
               <FaPhone />
             </button>
-            <button className="icon" data-label="password" onMouseOver={handleValue}>
+            <button
+              className="icon"
+              data-label="password"
+              onMouseOver={handleValue}
+            >
               <FaLock />
             </button>
           </div>
           <button className="btn" type="button">
-            {loading ? "loading...": "random-user"}
+            {loading ? "loading..." : "random-user"}
           </button>
         </div>
       </div>
