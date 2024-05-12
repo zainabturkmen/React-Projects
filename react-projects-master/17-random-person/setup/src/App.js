e3310b6958a84b59a49ff93da70e8fb37171d721
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-12 05:31:07 +0430

    random-person

--- react-projects-master/17-random-person/setup/src/App.js
+++ react-projects-master/17-random-person/setup/src/App.js
@@ -28,6 +28,7 @@ function App() {
           <img src={(person && person.image) || defaultImage} alt="random user" className="user-img" />
           <p className="user-title">my {title} is</p>
           <p className="user-value">{value}</p>
+          <div className="values-list"></div>
         </div>
       </div>
     </main>
