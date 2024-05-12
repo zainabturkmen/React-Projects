a0226d1d8b0acc186706ed0049d786f32c67e6a2
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-12 13:52:13 +0430

    random-person

--- react-projects-master/18-pagination/setup/src/App.js
+++ react-projects-master/18-pagination/setup/src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 
 function App() {
-  const {loading, data} = useFetch();
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+
+  return <main className="section-title">
+    <h1>{loading? "loading...":"pagination"}</h1>
+  </main>;
 }
 
-
-export default App  
+export default App;
