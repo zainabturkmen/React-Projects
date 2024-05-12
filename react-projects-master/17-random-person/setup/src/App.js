af75598b12e9ef62b400fac64daaa41a566afc0a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-12 11:16:41 +0430

    random-person

--- react-projects-master/17-random-person/setup/src/App.js
+++ react-projects-master/17-random-person/setup/src/App.js
@@ -55,6 +55,7 @@ function App() {
   const handleValue = (e) => {
     if (e.target.classList.contains("icon")) {
       const newValue = e.target.dataset.label;
+      setTitle(newValue)
       setValue(person[newValue])
     }
   };
