0188b7cd3201509e4e39bcdfd0be89af8d218b35
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-13 16:16:36 +0430

    pagination

--- react-projects-master/18-pagination/setup/src/App.js
+++ react-projects-master/18-pagination/setup/src/App.js
@@ -5,29 +5,27 @@ import Follower from "./Follower";
 function App() {
   const { loading, data } = useFetch();
   const [page, setPage] = useState(0);
-  const [followers, setFollowers] = useState([])
+  const [followers, setFollowers] = useState([]);
 
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading]);
 
-
-  useEffect(()=> {
-    if(loading) return
-    setFollowers(data[page])
-  },[loading])
-
-  return <main className="section-title">
-    <h1>{loading? "loading...":"pagination"}</h1>
-    <div className="underline"></div>
-    <section className="followers">
-      <div className="container">
-        {followers.map((follower)=> {
-          return <Follower  key={follower.id} {...follower} />
-        })}
-      </div>
-      <div className="btn-container">
-        hello world
-      </div>
-    </section>
-  </main>;
+  return (
+    <main className="section-title">
+      <h1>{loading ? "loading..." : "pagination"}</h1>
+      <div className="underline"></div>
+      <section className="followers">
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+        {!loading && <div className="btn-container">hello world</div>}
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- react-projects-master/18-pagination/setup/src/utils.js
+++ react-projects-master/18-pagination/setup/src/utils.js
@@ -1,7 +1,7 @@
 const paginate = (followers) => {
     const itemPerPage = 9;
     const pages = Math.ceil(followers.length / itemPerPage)
-    console.log(page);
+    console.log(pages);
 
     const newFollowers = Array.from({length:pages}, (_,index)=> {
         const start = index * itemPerPage;
