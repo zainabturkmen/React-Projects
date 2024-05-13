5c8a6cfbd880ee518496fabbfe77e04faddee2ad
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-13 16:26:04 +0430

    pagination

--- react-projects-master/18-pagination/setup/src/App.js
+++ react-projects-master/18-pagination/setup/src/App.js
@@ -4,18 +4,20 @@ import Follower from "./Follower";
 
 function App() {
   const { loading, data } = useFetch();
-  const [page, setPage] = useState(0);
+  const [pages, setPage] = useState(0);
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
     if (loading) return;
-    setFollowers(data[page]);
+    setFollowers(data[pages]);
   }, [loading]);
 
   return (
-    <main className="section-title">
-      <h1>{loading ? "loading..." : "pagination"}</h1>
-      <div className="underline"></div>
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "pagination"}</h1>
+        <div className="underline"></div>
+      </div>
       <section className="followers">
         <div className="container">
           {followers.map((follower) => {
