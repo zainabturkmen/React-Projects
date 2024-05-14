ff4ddfd4e64af8efd0452d38a92eb559c91097d0
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-14 14:07:25 +0430

    pagination

--- react-projects-master/18-pagination/setup/src/App.js
+++ react-projects-master/18-pagination/setup/src/App.js
@@ -16,15 +16,16 @@ function App() {
     setPage(index);
   };
 
-  const nextPage = ()=> {
-    setPage((oldPage)=> {
+  const nextPage = () => {
+    setPage((oldPage) => {
       let nextPage = oldPage + 1;
       if (nextPage > data.length - 1) {
         nextPage = 0;
       }
-    })
-  }
 
+      return nextPage;
+    });
+  };
 
   return (
     <main>
