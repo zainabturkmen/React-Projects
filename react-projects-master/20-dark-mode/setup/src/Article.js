748e4447f13ee3c467b2672d66f95749a2f8eaab
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-16 07:39:54 +0430

    dark-mode

--- react-projects-master/20-dark-mode/setup/src/Article.js
+++ react-projects-master/20-dark-mode/setup/src/Article.js
@@ -1,10 +1,11 @@
-import React from "react";
 import moment from "moment";
 const Article = ({ title, snippet, date, length }) => {
   return (
     <article className="post">
       <h2>{title}</h2>
-      <div className="post-info"></div>
+      <div className="post-info">
+        <span>date</span>
+      </div>
     </article>
   );
 };
