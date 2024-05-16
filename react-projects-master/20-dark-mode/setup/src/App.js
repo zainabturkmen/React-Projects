7bbe293b9231f9375597fef7c7d162faee0cee99
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-16 08:44:02 +0430

    changing custom colors

--- react-projects-master/20-dark-mode/setup/src/App.js
+++ react-projects-master/20-dark-mode/setup/src/App.js
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
 
 function App() {
-  return <main>
-    <nav>
-      <div className="nav-center">
-        <h1>overreacted</h1>
-        <button className="btn">toggle</button>
-      </div>
-    </nav>
-    <section className="articles">
-      {data.map((item) => {
-        return <Article key={item.id} {...item} />
-      })}
-    </section>
-  </main>
+  const [theme, setTheme]
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn">toggle</button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
