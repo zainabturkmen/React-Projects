e62e6ed39cb72862aab3725ef41a75f7319c7a3c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-16 07:37:12 +0430

    dark-mode

--- react-projects-master/20-dark-mode/setup/src/Article.js
+++ react-projects-master/20-dark-mode/setup/src/Article.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import moment from 'moment'
-const Article = ({title, snippet, date, }) => {
-  return 
-}
+import React from "react";
+import moment from "moment";
+const Article = ({ title, snippet, date, length }) => {
+  return <article className="post">
+    <h2>{title}</h2>
+    <div className="post-info"></div>
+  </article>;
+};
 
-export default Article
+export default Article;
