cca8f560dc871125c9640eef5fc56098d45d2390
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-16 09:06:10 +0430

    dark-mode

--- react-projects-master/20-dark-mode/setup/src/App.js
+++ react-projects-master/20-dark-mode/setup/src/App.js
@@ -12,7 +12,7 @@ const getStorageTheme = () => {
 }
 
 function App() {
-  const [theme, setTheme] = useState("light-theme");
+  const [theme, setTheme] = useState(getStorageTheme());
 
   const toggleTheme = () => {
     if (theme === "light-theme") {
