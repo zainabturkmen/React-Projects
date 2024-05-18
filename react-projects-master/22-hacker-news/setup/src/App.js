72622f2ce91cf36077252f4e057aed0acfeae549
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-18 09:01:35 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/App.js
+++ react-projects-master/22-hacker-news/setup/src/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import SearchForm from './SearchForm'
-import Stories from './Stories'
-import Buttons from './Buttons'
+import React from "react";
+import SearchForm from "./SearchForm";
+import Stories from "./Stories";
+import Buttons from "./Buttons";
+
 function App() {
+  return <>
+    <SearchForm />
+    <Buttons />
+    <Stories />
+  </>;
 }
 
-export default App
+export default App;
