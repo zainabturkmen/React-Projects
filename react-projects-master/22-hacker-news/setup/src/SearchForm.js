6008b3f5482c0fca5b8acf2ec621a97f2bafd959
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-18 18:52:12 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/SearchForm.js
+++ react-projects-master/22-hacker-news/setup/src/SearchForm.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
-  const {query, handleSearch} = useGlobalContext()
-  return <form className="search-form">
-    <h2>search hacker news</h2>
-  </form>
-}
+  const { query, handleSearch } = useGlobalContext();
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>search hacker news</h2>
+    </form>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
