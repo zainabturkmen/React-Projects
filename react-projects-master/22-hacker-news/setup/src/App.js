2194ddc2dbeb5a9189c62cff4604c8c1fa3469ff
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-18 09:00:39 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/App.js
+++ react-projects-master/22-hacker-news/setup/src/App.js
@@ -3,7 +3,6 @@ import SearchForm from './SearchForm'
 import Stories from './Stories'
 import Buttons from './Buttons'
 function App() {
-  return <h2>hacker news starter</h2>
 }
 
 export default App
--- react-projects-master/22-hacker-news/setup/src/context.js
+++ react-projects-master/22-hacker-news/setup/src/context.js
@@ -11,7 +11,7 @@ import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
-
+console.log(API_ENDPOINT);
 
 const initialState = {
   isLoading: true,
