e68e8096fcf0af7e0658aac9c1095e946d574d9d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-18 12:42:28 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/reducer.js
+++ react-projects-master/22-hacker-news/setup/src/reducer.js
@@ -11,7 +11,12 @@ const reducer = (state, action) => {
     case SET_LOADING:
       return { ...state, isLoading: true };
     case SET_STORIES:
-      return { ...state, isLoading: false };
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPags,
+      };
 
     default:
       throw new Error(`no matching"${action.type}" action type`);
