b4d54f3cd0132e8cdd882353ae410cace0de302a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-18 09:42:25 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/reducer.js
+++ react-projects-master/22-hacker-news/setup/src/reducer.js
@@ -8,10 +8,10 @@ import {
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "SET_LOADING":
+    case SET_LOADING:
       return { ...state, isLoading: true };
     default:
-      throw new Error(`no matching"${action.type}" action`)
+      throw new Error(`no matching"${action.type}" action type`);
   }
 };
 export default reducer;
