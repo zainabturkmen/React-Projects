8d96d838db472ecbfc560015f99144a730a70813
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-18 18:54:39 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/SearchForm.js
+++ react-projects-master/22-hacker-news/setup/src/SearchForm.js
@@ -6,6 +6,12 @@ const SearchForm = () => {
   return (
     <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>search hacker news</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
     </form>
   );
 };
