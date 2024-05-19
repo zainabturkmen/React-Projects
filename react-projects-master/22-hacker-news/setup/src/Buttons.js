446df47f430328f62504a63338ececa2a144d128
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-19 07:57:40 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/Buttons.js
+++ react-projects-master/22-hacker-news/setup/src/Buttons.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 
-const Buttons = () => {
-  return <h2>button container</h2>
-}
+const Buttons = ({ isLoading, page, nbPages, handlePage }) => {
+  return <h2>button container</h2>;
+};
 
 export default Buttons
