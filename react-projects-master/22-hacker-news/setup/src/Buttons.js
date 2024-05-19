cbe67a82c15ca1818510d71990a27e07129af30d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-19 08:18:55 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/Buttons.js
+++ react-projects-master/22-hacker-news/setup/src/Buttons.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useGlobalContext } from "./context";
 
-const Buttons = ({ isLoading, page, nbPages, handlePage }) => {
+const Buttons = () => {
+  const { isLoading, page, nbPages, handlePage } = useGlobalContext()
   return (
     <div className="btn-container">
       <button disabled={isLoading} onClick={() => handlePage("dec")}>
