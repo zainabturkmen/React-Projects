680c7c8587717f766e7b71a43edbb50f7214be15
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-19 08:42:51 +0430

    hacker news

--- react-projects-master/22-hacker-news/setup/src/reducer.js
+++ react-projects-master/22-hacker-news/setup/src/reducer.js
@@ -24,7 +24,8 @@ const reducer = (state, action) => {
       };
     case HANDLE_SEARCH:
       return { ...state, query: action.payload, page: 0 };
-
+    case  HANDLE_PAGE:
+      {...state, }
     default:
       throw new Error(`no matching"${action.type}" action type`);
   }
